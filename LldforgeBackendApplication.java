cc715451d90dfac268bd9efab3d8e71f26906fd8
AMIT KUMAR SHAW <shawamit20037890@gmail.com>
2026-01-03 12:46:41 +0530

    Add files via upload

---
+++ LldforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devmare.lldforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableCaching
+@SpringBootApplication
+public class LldforgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldforgeBackendApplication.class, args);
+    }
+}
